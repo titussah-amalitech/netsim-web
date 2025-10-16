a5a3855ffbe750c7fab28070915accb99d754afa
Donatus Zagla <beneesolomon765@gmail.com>
2025-10-16 12:51:50 +0000

    WIP: updated GamePage alert logic

--- src/features/game-simulation/views/GamePage.jsx
+++ src/features/game-simulation/views/GamePage.jsx
@@ -9,7 +9,7 @@ export const GamePage = () => {
   
 
   const devices = [
-   { name: "Router-01", status: "yellow" },
+   // { name: "Router-01", status: "yellow" },
    { name: "Switch-02", status: "red" },
   ];
 
